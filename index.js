b7aeb8c0e356c02f4c402344cf0d5382d32dd3dc
Yuliya <yulya.kukareko@mail.ru>
2018-09-11 17:21:41 +0300

    Add function for iteration directories and files

--- index.js
+++ index.js
@@ -10,3 +10,27 @@ for(var i = 0; i<process.argv.length; i++) {
     console.log(`next item: ${process.argv[i]} \n`);
 }
 //endregion
+
+
+//region Task_3
+var fs = require('fs');
+var path = require('path');
+
+var getFiles = function (dir, files_){
+
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files){
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name, files_);
+        } else {
+            files_.push(name.replace(/\.\/folder\//g,''));
+        }
+    }
+    return files_;
+};
+
+
+console.log(getFiles('./folder'));
+//endregion
