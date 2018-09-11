ed2e3fc42086aa980a5bfe1c6000e5bcad827743
Yuliya <yulya.kukareko@mail.ru>
2018-09-11 16:29:05 +0300

    print all command line arguments

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+//region Task_1
 const name = process.argv[2];
 
 console.log(`Hi ${name}!`);
+//endregion
+
+
+//region Task_2
+for(var i = 0; i<process.argv.length; i++) {
+    console.log(`next item: ${process.argv[i]} \n`);
+}
+//endregion
