f15a7c4294bfff0a561a14a69b10670197c9b41c
Yuliya <yulya.kukareko@mail.ru>
2018-09-18 07:03:57 +0300

    Add function for create directory and write files

--- index.js
+++ index.js
@@ -16,43 +16,54 @@ for(var i = 0; i<process.argv.length; i++) {
 
 let fs = require('fs');
 
-let createScript = function( path, callback){
-    if( typeof callback !== 'function' )
-        return ;
-    let result = [], files = [ path.replace( /\/\s*$/, '' ) ];
-    function traverseFiles (){
-        if( files.length ) {
-            let name = files.shift();
-            fs.stat(name, function( err, stats){
-                if( err ){
-
-                    if( err.errno == 34 )
-                        traverseFiles();
-                    else callback(err)
-                }
-                else if ( stats.isDirectory())
-                    fs.readdir( name, function( err, files2 ){
-                    if( err )
-                        callback(err)
-                    else {
-                        files = files2
-                            .map( function( file ){ return name + '/' + file } )
-                            .concat( files )
-                        traverseFiles()
-                    }
-                });
-                else{
-                    result.push(name);
-                    traverseFiles()
-                }
-            })
-        }
-        else callback( null, result )
-    }
-    traverseFiles()
+let fileCreate = function (z) {
+    fs.open('./summary.js', 'w',(err, fd) => {});
+    fs.writeFile("./summary.js","let fs = require('fs'); \n let createScript = function( path, callback){\n" +
+        "    // the callback gets ( err, files) where files is an array of file names\n" +
+        "    if( typeof callback !== 'function' )\n" +
+        "        return ;\n" +
+        "    let result = [], files = [ path.rep" +
+        "lace( /\\/\\s*$/, '' ) ];\n" +
+        "    function traverseFiles (){\n" +
+        "        if( files.length ) {\n" +
+        "            let name = files.shift();\n" +
+        "            fs.stat(name, function( err, stats){\n" +
+        "                if( err ){\n" +
+        "\n" +
+        "                    if( err.errno == 34 )\n" +
+        "                        traverseFiles();\n" +
+        "                    else callback(err)\n" +
+        "                }\n" +
+        "                else if ( stats.isDirectory())\n" +
+        "                    fs.readdir( name, function( err, files2 ){\n" +
+        "                    if( err )\n" +
+        "                        callback(err)\n" +
+        "                    else {\n" +
+        "                        files = files2\n" +
+        "                            .map( function( file ){ return name + '/' + file } )\n" +
+        "                            .concat( files )\n" +
+        "                        traverseFiles()\n" +
+        "                    }\n" +
+        "                });\n" +
+        "                else{\n" +
+        "                    result.push(name);\n" +
+        "                    traverseFiles()\n" +
+        "                }\n" +
+        "            })\n" +
+        "        }\n" +
+        "        else callback( null, result )\n" +
+        "    }\n" +
+        "    traverseFiles()\n" +
+        "};\n" +
+        "\n" +
+        "createScript('./folder',function(filePath, stat) {\n" +
+        "    console.log(stat);\n" +
+        "});", (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
 };
 
-createScript('./folder',function(filePath, stat) {
-    console.log(stat);
-});
+
+fileCreate('');
 //endregion
--- summary.js
+++ summary.js
@@ -1,42 +1,67 @@
-var fs = require('fs');
-var path = require('path');
+let fs = require('fs'); 
+ let createScript = function( path, callback){
+    // the callback gets ( err, files) where files is an array of file names
+    if( typeof callback !== 'function' )
+        return ;
+    let result = [], files = [ path.replace( /\/\s*$/, '' ) ];
+    function traverseFiles (){
+        if( files.length ) {
+            let name = files.shift();
+            fs.stat(name, function( err, stats){
+                if( err ){
 
-var getFiles = function (dir,files_){
-
-    files_ = files_ || [];
-    var files = fs.readdir(dir, function (err, data) {
-        debugger;
-        for (var i in data){
-            debugger;
-            var name = dir + '/' + data[i];
-            if (fs.stat(name, function (err, data) {
-                return data;
-            }).isDirectory()){
-                getFiles(name, files_);
-            } else {
-                if(flag == false || typeof flag == 'undefined') {
-                    files_.push(name.replace(/\.\/folder\//g, ''));
+                    if( err.errno == 34 )
+                        traverseFiles();
+                    else callback(err)
                 }
-                else {
-                    files_.push(name);
+                else if ( stats.isDirectory())
+                    fs.readdir( name, function( err, files2 ){
+                    if( err )
+                        callback(err)
+                    else {
+                        files = files2
+                            .map( function( file ){ return name + '/' + file } )
+                            .concat( files )
+                        traverseFiles()
+                    }
+                });
+                else{
+                    result.push(name);
+                    traverseFiles()
                 }
-            }
+            })
         }
-        return files_;
+        else callback( null, result )
+    }
+    traverseFiles()
+};
+
+createScript('./folder',function(filePath, stat) {
+    console.log(stat);
+});
+
+const path = require('path');
+
+let createDirectory = function(){
+    fs.mkdir('folder/folder',(err, succ) => {});
+    createScript('./folder',function(filePath, stat) {
+        for(let i=0;i<stat.length;i++){
+            console.log(stat[i]);
+            fs.readFile(stat[i],"utf-8", (err, data) => {
+                console.log(data);
+                let nameFile = path.basename(stat[i]);
+                console.log(nameFile);
+                if (err)
+                    throw err;
+                fs.open('folder/folder/'+nameFile, 'w',(err, fd) => {});
+                fs.writeFile('./folder/folder/'+nameFile, data,(err) => {
+                    if (err)
+                        throw err;
+                    console.log('The file has been saved!');
+                });
+            });
+        }
     });
 };
 
-var createOtherDirectory = function () {
-    var arrayPath = getFiles('./folder',true);
-    for(var i = 0; i<arrayPath.length;i++) {
-        fs.readFile(arrayPath[i], "utf8",
-            function (error, data) {
-                if (error)
-                    throw error;
-                console.log(data);
-            });
-    }
-}
-
-
-console.log(getFiles('./folder'));
+createDirectory();
